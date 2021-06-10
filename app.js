66a2425acba8722cf5a9520a12b9b7ce660da399
darren <darren__c@hotmail.com>
2021-06-11 02:15:37 +0900

    sending a response

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const http = require("http");
 // SIDE NOTE : (./ is a relative path and / is an absolute path)
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req, req.url, res.method, req.headers);
+
+  res.setHeader("Content", "text/html");
+  res.write("<h1>Hello from the server</h1>");
+  res.end();
 });
 
 server.listen(3000);
